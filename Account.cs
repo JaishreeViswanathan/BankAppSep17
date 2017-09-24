71c7aae5265b4f619a9c676f29a4004ef1de8eca
Jaishree Viswanathan <Jaishree.viswanathan@gmail.com>
2017-09-24 11:53:33 -0700

    Add files via upload

---
+++ Account.cs
@@ -0,0 +1,74 @@
+﻿using System;
+namespace BankApp
+{
+    /// <summary>
+    /// Account.
+    /// </summary>
+    public class Account
+    {
+
+        private static int lastAccountNumber = 0;
+
+        #region Properties
+        /// <summary>
+        /// Gets or sets the account number.
+        /// </summary>
+        /// <value>The account number.</value>
+
+        public int AccountNumber
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Gets or sets the email address.
+        /// </summary>
+        /// <value>The email address.</value>
+
+        public string EmailAddress
+        {
+            get;
+            set;
+        }
+        public decimal Balance
+        {
+            get;
+            private set;
+        }
+        public string AccountType
+        {
+            get;
+            set;
+        }
+        public DateTime CreatedDate
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+
+        public Account()
+        {
+            AccountNumber = ++lastAccountNumber;
+        }
+
+        #endregion
+
+
+        #region Methods
+
+        public decimal Deposit(decimal amount)
+        {
+            Balance += amount;
+            return Balance;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            Balance-= amount;
+        }
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BankApp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Instantiate an object
+            var account = new Account();
+            //lhs is a variable mem allocation, so I can come back to it.
+            //account.AccountNumber = 1234;
+            account.EmailAddress = "test@mailinator.com";
+            account.AccountType = "Savings";
+            //account.Balance = 10000000000000.00;
+
+            //compile time calculation of datatype.
+
+            var newBalance= account.Deposit(100);
+            //Inline referencing
+            Console.WriteLine($"AN: {account.AccountNumber}, EA: {account.EmailAddress}, Balance: {account.Balance:C}, AT: {account.AccountType}");
+
+            var account2 = new Account();
+			Console.WriteLine($"AN: {account2.AccountNumber}, EA: {account2.EmailAddress}, Balance: {account2.Balance:C}, AT: {account2.AccountType}");
+        
+        }
+    }
+}
